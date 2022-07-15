cd5b14b0bf9f2aa97618c18d00883712e953abec
Justin Vongdara <vongdara16@gmail.com>
2022-07-15 11:22:24 -0400

    functioning game. score tracker needs work

--- py-pac-poe.py
+++ py-pac-poe.py
@@ -28,7 +28,11 @@
   # also the turn variable
 
 board = {}
-turn = 'X'
+turn = 'O'
+x_win = 0
+o_win = 0
+move = None
+choice_list = ['a1','b1','c1','a2','b2','c2','a3','b3','c3']
 welcome = "------------------------\nLet's Play Py-Pac-Poe!!!\n------------------------"
 
 # Game Init
@@ -45,21 +49,9 @@ def init():
     'a3': ' ', 'b3': ' ', 'c3': ' '
   }
 
-  display_board = f'''
-      A   B   C 
-  1)  {board['a1']} | {board['b1']} | {board['c1']} 
-     -----------
-  2)  {board['a2']} | {board['b2']} | {board['c2']} 
-     -----------
-  3)  {board['a3']} | {board['b3']} | {board['c3']} 
-  '''
-  
-  print(display_board)
+  # print(f"\nIt is Player {turn}'s turn!") # maybe move this print
 
-  print(f"It is Player {turn}'s turn!")
-
-init()
-
+# init()
 
 # Change Turns
   # this will change the turn variable
@@ -67,13 +59,59 @@ init()
 def change_turns():
   global turn
   turn = 'X' if turn == 'O' else 'O'
+  print(f"\nIt is Player {turn}'s turn!")
+
+# Display Board
+  # displays game board
+def display_board():
+  global board
+  board_display = f'''
+      A   B   C 
+  1)  {board['a1']} | {board['b1']} | {board['c1']} 
+     -----------
+  2)  {board['a2']} | {board['b2']} | {board['c2']} 
+     -----------
+  3)  {board['a3']} | {board['b3']} | {board['c3']} 
+  '''
+
+  print(board_display)
+
+# display_board(board)
+
+# Get Move
+  # this will get the players move
+
+def get_move():
+  # global turn # might not need since we aren't modifying it
+  global move
+  move = input(f"Player {turn}, please select a move: ").lower()
+  # return move
+
+# Check Move
+  # checks if the move is valid
+
+def check_move():
+  global board, move, choice_list
+  # choice_list = ['a1','b1','c1','a2','b2','c2','a3','b3','c3']
+  if move in choice_list:
+    return True
+  else: 
+    print('not a valid move')
+    # get_move()
+    return False
 
 # Set Player Move
   # this will set the move on the board dictionary
   # it will call change turns 
 
 def player_move():
-  global board
+  global board, turn, move, choice_list
+  # if check_move():
+  #   board[move] = turn
+  board[move] = turn
+  choice_list.remove(move)
+
+
 
 
 # Check Win
@@ -81,17 +119,72 @@ def player_move():
   # this will check by checking the spaces in the board
   # this will return true or false
 
+def check_win():
+  global board
+  # print(board)
+  if ((board['a1'] == board['b1']) and (board['a1'] == board['c1'])):
+    if not(board['a1'] == ' '):
+      print('124')
+      return True
+  elif ((board['a1'] == board['a2']) and (board['a1'] == board['a3'])):
+    if not(board['a1'] == ' '):
+      print('127')
+      return True
+  elif ((board['b1'] == board['b2']) and (board['b1'] == board['b3'])):
+    if not(board['b1'] == ' '):
+      print('130')
+      return True
+  elif ((board['c1'] == board['c2']) and (board['c1'] == board['c3'])):
+    if not(board['c1'] == ' '):
+      print('133')
+      return True
+  elif ((board['a2'] == board['b2']) and (board['a2'] == board['c2'])):
+    if not(board['a2'] == ' '):
+      print('136')
+      return True
+  elif ((board['a3'] == board['b3']) and (board['a3'] == board['c3'])):
+    if not(board['a3'] == ' '):
+      print('139')
+      return True
+  elif ((board['a1'] == board['b2']) and (board['a1'] == board['c3'])):
+    if not(board['a1'] == ' '):
+      print('142')
+      return True
+  elif ((board['a3'] == board['b2']) and (board['a3'] == board['c1'])): 
+    if not(board['a3'] == ' '):
+      print('145')
+      return True
+  
 
 
 
+def play_game():
+  global x_win, o_win
+  init()
+  display_board()
 
+  for x in range(9):
+    change_turns()
+    get_move()
+    while not(check_move()):
+      get_move()
+    player_move()
+    display_board()
+    
+    if x > 3:
+      if check_win():
+        print(f"Player {turn} Won!")
+        if turn == 'X':
+          x_win += 1
+        else: 
+          o_win += 1
+        print(f"Player X wins: {x_win} | Player O wins: {o_win}")
+        break
 
+  if not(check_win()):
+    print('TIE!')
 
-
-
-
-
-
+play_game()
 
 
 
